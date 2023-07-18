0bf0b7dfbaa12ec33ef3293806f22918a76e058f
s1f10210368 <s1f102103684@iniad.org>
2023-07-18 21:13:51 +0900

    R

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,62 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
+  const [maze, setmaze] = useState([
+    [0, 0, 0], //0が通れる場所
+    [0, 0, 0], //1が柱または柱が倒れた場所
+    [0, 0, 0],
+  ]);
+
+  const directions = [
+    [-1, 0], //左
+    [1, 0], //右
+    [0, -1], //上
+    [0, 1], //下
+  ];
+
+  const [human, sethuman] = useState();
+
+  const maketoweratodd = (x: number, y: number) => {
+    //x, yともに奇数の場所に柱を生成(maze上)
+    for (let x = 0; x < maze.length; x++) {
+      for (let y = 0; y < maze[x].length; y++) {
+        if (x % 2 !== 0 && y % 2 !== 0) {
+          const updatemaze = [...maze];
+          updatemaze[x][y] = 1;
+          setmaze(updatemaze);
+          console.table(maze);
+        }
+      }
+    }
+  };
+
+  const falldowntower = (x: number, y: number) => {
+    maketoweratodd(x, y);
+    for (const [dx, dy] of directions) {
+      const newX = x + dx;
+      const newY = y + dy;
+    }
+  };
+
+  const firstclick = (x: number, y: number) => {
+    //迷路を生成
+  };
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      {['生成'].map((v) => (
+        <li onClick={maketoweratodd} key={v}>
+          (生成)
+        </li>
+      ))}
+      <div className={styles.maze}>
+        {maze.map((row, x) =>
+          row.map((cell, y) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => onClick(x, y)} />
+          ))
+        )}
+      </div>
     </div>
   );
 };
