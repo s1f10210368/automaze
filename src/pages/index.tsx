c091ba34bd6cae1da11bc3bdd279ef9f76109f01
s1f10210368 <s1f102103684@iniad.org>
2023-07-18 22:34:57 +0900

    R

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,58 +2,72 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [maze, setmaze] = useState([
-    [0, 0, 0], //0が通れる場所
-    [0, 0, 0], //1が柱または柱が倒れた場所
-    [0, 0, 0],
+  const [maze, setMaze] = useState([
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
   ]);
 
-  const directions = [
-    [-1, 0], //左
-    [1, 0], //右
-    [0, -1], //上
-    [0, 1], //下
+  const front = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
   ];
 
-  const [human, sethuman] = useState();
-
-  const maketoweratodd = (x: number, y: number) => {
-    //x, yともに奇数の場所に柱を生成(maze上)
+  const maketoweratodd = () => {
+    const startCells: number[][] = [];
     for (let x = 0; x < maze.length; x++) {
       for (let y = 0; y < maze[x].length; y++) {
-        if (x % 2 !== 0 && y % 2 !== 0) {
-          const updatemaze = [...maze];
-          updatemaze[x][y] = 1;
-          setmaze(updatemaze);
-          console.table(maze);
+        if (x % 2 === 1 && y % 2 === 1) {
+          startCells.push([x, y]);
+          const updatedMaze = [...maze];
+          updatedMaze[x][y] = 1;
+          setMaze(updatedMaze);
         }
       }
     }
+    return startCells;
   };
 
-  const falldowntower = (x: number, y: number) => {
-    maketoweratodd(x, y);
-    for (const [dx, dy] of directions) {
+  const puttoweraroundodd = () => {
+    const startCells = maketoweratodd();
+    const updatemaze = [...maze];
+    for (const startCell of startCells) {
+      const [x, y] = startCell;
+
+      //ランダム方向選択
+      const randomDirectionIndex = Math.floor(Math.random() * 4);
+      const randomDirection = front[randomDirectionIndex];
+      const [dx, dy] = randomDirection;
+
+      //選択した方向のセルが迷路ないならそのセルの値を1に変更
       const newX = x + dx;
       const newY = y + dy;
-    }
-  };
 
-  const firstclick = (x: number, y: number) => {
-    //迷路を生成
+      if (newX >= 0 && newX < maze.length && newY >= 0 && newY < maze[0].length) {
+        updatemaze[newX][newY] = 1;
+      }
+    }
+    setMaze(updatemaze);
   };
 
   return (
     <div className={styles.container}>
       {['生成'].map((v) => (
         <li onClick={maketoweratodd} key={v}>
-          (生成)
+          {v}
         </li>
       ))}
+
       <div className={styles.maze}>
         {maze.map((row, x) =>
           row.map((cell, y) => (
-            <div className={styles.cell} key={`${x}-${y}`} onClick={() => onClick(x, y)} />
+            <div className={styles.cell} key={`${x}-${y}`}>
+              {cell === 1 && <div className={styles.pillar} />}
+            </div>
           ))
         )}
       </div>
