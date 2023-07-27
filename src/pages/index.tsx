fe967d1a75dd96618e2b3e2b85ee87b89fc196e5
s1f10210368 <s1f102103684@iniad.org>
2023-07-28 00:05:41 +0900

    R

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home = () => {
     [-1, 0], //up
   ];
 
-  const [human, setHuman] = useState({
+  const [human, sethuman] = useState({
     x: 0,
     y: 0,
     //初期値下向き
@@ -35,14 +35,97 @@ const Home = () => {
     return false;
   };
 
-  const change;
-
   const movehuman = () => {
-    if (human.y - 1 < 0 || maze[human.x][human.y - 1] === 1) {
-      if (human.x + 1 > 8 || maze[human.x + 1][human.y] === 1) {
-        human.front;
+    const right = rightdirection();
+    // 右側の値を確認
+    if (right[0] !== 0) {
+      //進行方向が上下どちらかの場合
+      changedirectionupdown(right[0]);
+    } else {
+      changedirectionleftright(right[1]);
+    }
+  };
+
+  const checkupdown = (num: number) => {
+    if (human.x + num < 0 || human.x + num > 8 || maze[human.x + num][human.y] === 1) {
+      return true;
+    }
+    return false;
+  };
+
+  const changedirectionupdown = (num: number) => {
+    if (checkupdown(num)) {
+      // 壁だよ
+      checkfront();
+    } else {
+      // 向きの変更
+      sethuman({ x: human.x, y: human.y, front: rightdirection() });
+    }
+  };
+
+  const checkleftright = (num: number) => {
+    if (human.y + num < 0 || human.y + num > 8 || maze[human.x][human.y + num] === 1) {
+      return true;
+    }
+    return false;
+  };
+
+  const changedirectionleftright = (num: number) => {
+    if (checkleftright(num)) {
+      // 壁だよ
+      checkfront();
+    } else {
+      // 向きの変更
+      sethuman({ x: human.x, y: human.y, front: rightdirection() });
+    }
+  };
+
+  const rightdirection = () => {
+    if (JSON.stringify(human.front) === JSON.stringify([0, 1])) {
+      // 右が正面の場合、下
+      return [1, 0];
+    } else if (JSON.stringify(human.front) === JSON.stringify([1, 0])) {
+      // 下が正面の場合、左
+      return [0, -1];
+    } else if (JSON.stringify(human.front) === JSON.stringify([0, -1])) {
+      // 左が正面の場合、上
+      return [-1, 0];
+    } else {
+      // 上が正面の場合、右
+      return [0, 1];
+    }
+  };
+
+  const changedirection = () => {
+    if (JSON.stringify(human.front) === JSON.stringify([0, 1])) {
+      // 右が正面の場合、上
+      sethuman({ x: human.x, y: human.y, front: [-1, 0] });
+    } else if (JSON.stringify(human.front) === JSON.stringify([1, 0])) {
+      // 下が正面の場合、右
+      sethuman({ x: human.x, y: human.y, front: [0, 1] });
+    } else if (JSON.stringify(human.front) === JSON.stringify([0, -1])) {
+      // 左が正面の場合、下
+      sethuman({ x: human.x, y: human.y, front: [1, 0] });
+    } else {
+      // 上が正面の場合、左
+      sethuman({ x: human.x, y: human.y, front: [0, -1] });
+    }
+  };
+
+  const checkfront = () => {
+    // 正面の確認
+    if (human.front[0] !== 0) {
+      if (checkupdown(human.front[0]) === false) {
+        sethuman({ x: human.x + human.front[0], y: human.y, front: human.front });
+      } else {
+        changedirection();
       }
     } else {
+      if (checkleftright(human.front[1]) === false) {
+        sethuman({ x: human.x, y: human.y + human.front[1], front: human.front });
+      } else {
+        changedirection();
+      }
     }
   };
 
@@ -91,7 +174,7 @@ const Home = () => {
         </li>
       ))}
       {['探索'].map((v) => (
-        <li onClick={searchhuman} key={v}>
+        <li onClick={movehuman} key={v}>
           {v}
         </li>
       ))}
@@ -101,7 +184,18 @@ const Home = () => {
           row.map((cell, y) => (
             <div className={styles.cell} key={`${x}-${y}`}>
               {cell === 1 && <div className={styles.pillar} />}
-              {searchhuman(x, y) === true && '▼'}
+              {searchhuman(x, y) === true &&
+                JSON.stringify(human.front) === JSON.stringify([0, 1]) &&
+                '▶︎'}
+              {searchhuman(x, y) === true &&
+                JSON.stringify(human.front) === JSON.stringify([1, 0]) &&
+                '▼'}
+              {searchhuman(x, y) === true &&
+                JSON.stringify(human.front) === JSON.stringify([0, -1]) &&
+                '◀︎'}
+              {searchhuman(x, y) === true &&
+                JSON.stringify(human.front) === JSON.stringify([-1, 0]) &&
+                '▲'}
             </div>
           ))
         )}
